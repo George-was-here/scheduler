82718c237f7db278dfaee94867e30ea2f34b8462
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-18 00:55:33 -0400

    Implementing DayListItem prop based styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+
+import classNames from "classnames";
+
 export default function DayListItem(props) {
   const dayName = props.name;
+  const dayListClassName = classNames('dayClass day-list__item', { "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0});
+
   return (
-    <li onClick={() => props.setDay(dayName)}>
+    <li className={dayListClassName} onClick={() => props.setDay(dayName)}>
       <h2 className="text--regular">{dayName}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
