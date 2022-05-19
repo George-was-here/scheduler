967e9855e1f933db10cd0536768c853897687193
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-19 18:19:21 -0400

    Adding story for Empty component onClick functionality

--- stories/index.js
+++ stories/index.js
@@ -19,6 +19,8 @@ import Appointment from "components/Appointment/index.js";
 
 import Header from "components/Appointment/Header"
 
+import Empty from "components/Appointment/Empty"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -151,4 +153,6 @@ storiesOf("Button", module)
     })
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time={"12pm"} />)
-    .add("Header", () => <Header time="12pm" />);
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAddd")} />);
+
