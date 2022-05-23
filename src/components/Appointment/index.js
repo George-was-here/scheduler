cd1842e23f15ba6d0e9a01ff912ea6b43ba03794
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-23 01:00:48 -0400

    Adding hook to Appointment component to render modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React,  { useEffect } from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import { useVisualMode } from "hooks/useVisualMode";
 
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment (props) {
-
+  const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
+  
   return (
-  <article className="appointment">
+    <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student}/> : <Empty />}
-  </article>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          student={props.interview ? props.interview.student: null}
+          interviewer={props.interview ? props.interview.interviewer: null}
+          onAdd={() => {}}
+          onCancel={back}
+          interviewers={[]}
+        />
+      )}
+    </article>
   );
-  }
+};
