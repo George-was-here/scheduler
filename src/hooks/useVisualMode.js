6e0c440e660e1d4da5d1f85beef5433386e104d7
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-23 00:01:52 -0400

    Implementing transition function and testing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 
 export function useVisualMode(modeOrg) {
 const [mode, setMode] = useState(modeOrg);
+
+function transition(newMode) {
+  setMode(newMode)
+}
 return {
-  mode
+  mode,
+  transition
 }
 };
 
