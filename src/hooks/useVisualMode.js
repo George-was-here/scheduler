df15d87d5183306cd99592b4eaf6e9daaaf7daf6
George Burt <georgeburt@Georges-MacBook-Air.local>
2022-05-23 00:21:27 -0400

    Implementing back function and testing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";  
 
-
 export function useVisualMode(modeOrg) {
 const [mode, setMode] = useState(modeOrg);
+const [history, setHistory] = useState([modeOrg]);
 
 function transition(newMode) {
-  setMode(newMode)
+  // set the new mode state
+  setMode(newMode);
+  // add new mode to history state
+  setHistory([...history, newMode])
 }
+
+function back() {
+  const newHistory = history;
+  // remove the last element
+  if(newHistory.length > 1) {
+    newHistory.pop();
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1]);
+  }
+}
+
 return {
   mode,
-  transition
+  transition,
+  back
 }
 };
-
